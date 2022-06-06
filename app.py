9011a880b86bf34a527764878fad5f5f6cc90353
Nicolas <nicolas_mcz@hotmail.com>
2022-06-06 00:04:25 -0300

    docker

--- app.py
+++ app.py
@@ -13,27 +13,26 @@ app.layout = html.Div(
     children=[
         html.H1(children="Hello Dash 2020"),
         html.Div(children="""Dash: A web application framework for Python."""),
-        # dcc.Graph(
-        #     id="example-graph",
-        #     figure={
-        #         "data": [
-        #             {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "SF"},
-        #             {
-        #                 "x": [1, 2, 3],
-        #                 "y": [2, 4, 5],
-        #                 "type": "bar",
-        #                 "name": u"Montréal",
-        #             },
-        #         ],
-        #         "layout": {"title": "Dash Data Visualization"},
-        #     },
-        # ),
+        dcc.Graph(
+            id="example-graph",
+            figure={
+                "data": [
+                    {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "SF"},
+                    {
+                        "x": [1, 2, 3],
+                        "y": [2, 4, 5],
+                        "type": "bar",
+                        "name": u"Montréal",
+                    },
+                ],
+                "layout": {"title": "Dash Data Visualization"},
+            },
+        ),
     ]
 )
 
 
 if __name__ == "__main__":
     import os
-
     debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
     app.run_server(host="0.0.0.0", port=8050, debug=debug)
